898f7303680e873cf1eefb80755d5c8c0ea27757
JHermansen321 <144226@student.asd20.org>
2018-12-07 14:39:34 -0700

    changes

---
+++ PlatForm Push/Assets/Border.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Border : MonoBehaviour
+{
+    public string levelToLoad;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+}
