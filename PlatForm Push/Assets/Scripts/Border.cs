70ae3b349913171614dcb57ffa038716ad16e8e7
JHermansen321 <144226@student.asd20.org>
2018-12-21 08:52:41 -0700

    Final Commit

---
+++ PlatForm Push/Assets/Scripts/Border.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Border : MonoBehaviour
+{
+    public string levelToLoad1;
+    public string levelToLoad2;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player1")
+        {
+            SceneManager.LoadScene(levelToLoad1);
+           
+        }
+        if(other.tag == "Player2")
+        {
+            SceneManager.LoadScene(levelToLoad2);
+           
+        }
+    }
+}
---
+++ PlatForm Push/Assets/Scripts/MenuButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{   
+        public string LevelName;      
+        public void OonClick()
+        {
+            SceneManager.LoadScene(LevelName);
+        }   
+}
